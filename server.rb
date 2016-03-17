b8808d4fcac6a2642ea1814883ff56ab2fbdf759
Chris Kelly <cpjkelly@gmail.com>
2016-03-17 13:14:07 -0700

    Move server to module

--- server.rb
+++ server.rb
@@ -3,26 +3,29 @@ require "json"
 require "pry"
 require "httparty"
 
-class SMTPServer < MiniSmtpServer
+module SMTP
   LISTEN_PORT = 1025
   LISTEN_IP = "127.0.0.1"
 
-  def new_message_event(message_hash)
-    puts "posting"
-    body = {
-      "email" => {
+  class Server < MiniSmtpServer
+
+    def new_message_event(message_hash)
+      puts "posting"
+      body = {
         "email" => {
-          "data" => message_hash[:data],
-          "sender_email" => message_hash[:from],
-          "receiver_email" => message_hash[:to]
+          "email" => {
+            "data" => message_hash[:data],
+            "sender_email" => message_hash[:from],
+            "receiver_email" => message_hash[:to]
+          }
         }
       }
-    }
-    HTTParty.post "http://localhost:4000/api/emails", body: body
-    puts "posted"
+      HTTParty.post "http://localhost:4000/api/emails", body: body
+      puts "posted"
+    end
   end
 end
 
-server = SMTPServer.new(LISTEN_PORT, LISTEN_IP, 4)
+server = SMTP::Server.new(SMTP::LISTEN_PORT, SMTP::LISTEN_IP, 4)
 server.start
 server.join
